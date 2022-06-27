914cf69f8265cfc9d8318282074cd961caee5a7e
Daniel Dmitry <dmitrj77@gmail.com>
2022-06-27 20:38:01 +0300

    demo1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,11 +5,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
     WebDriver driver;
 
     @BeforeSuite
-    static void setupClass() {
+    static void setup() {
         WebDriverManager.chromedriver().setup();
     }
 
@@ -17,7 +19,7 @@ public class BaseTest {
     void setupTest() {
         driver = new ChromeDriver();
         driver.get("http://www.foxtrot.com.ua");
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @AfterMethod
