73af5ce66bff1b7b154c4ec3574bdcfa573f77b1
Daniel Dmitry <dmitrj77@gmail.com>
2022-07-04 21:12:37 +0300

    checkFavoriteIcon

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -11,6 +12,8 @@ public class HomePage extends BasePage {
 
     private By searchInput = By.cssSelector(".header-search__field");
     private By languageChoice = By.xpath("//ul[@class='header__sub-lang']//li/a");
+    private By productCatalog=new By.ByXPath("//div[@class='tooltip slot tooltip_active']/div[@class='header-tooltip__nodata']/a");
+    private By favoriteIcon = new By.ByCssSelector(".header-favorite__icon.header-favorite__icon_hover.icon-heart-filled");
 
 
     public HomePage(WebDriver driver) {
@@ -27,12 +30,20 @@ public class HomePage extends BasePage {
 
     public HomePage setLanguage(String language) {
         List<WebElement> elementList = driver.findElements(languageChoice);
+        wait.until(ExpectedConditions.visibilityOfAllElements(elementList));
         if (elementList.get(0).getText().equals(language)) {
             elementList.get(0).click();
         } else if (elementList.get(1).getText().equals(language)) {
             elementList.get(1).click();
         }
         return new HomePage(driver);
+    }
+    public AllCategoriesPage favoriteAllCategory(){
+        WebElement searchCompareIcon=driver.findElement(favoriteIcon);
+        searchCompareIcon.click();
+        WebElement searchToCatalogButton=driver.findElement(productCatalog);
+        searchToCatalogButton.click();
+        return new AllCategoriesPage(driver);
     }
 
 }
