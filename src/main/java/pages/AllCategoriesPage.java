7eaf39050b339ac9665dbd2937a8707fda235a69
Daniel Dmitry <dmitrj77@gmail.com>
2022-07-04 20:20:23 +0300

    checkCompareIcon

--- src/main/java/pages/AllCategoriesPage.java
+++ src/main/java/pages/AllCategoriesPage.java
@@ -1,10 +1,18 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AllCategoriesPage extends BasePage {
+
+    private By pageTitle= new By.ByCssSelector(".page__title");
     public AllCategoriesPage(WebDriver driver) {
         super(driver);
+    }
+    public String getTitleAllCategoryPage(){
+        WebElement searchPageTitle=driver.findElement(pageTitle);
+      return   searchPageTitle.getText();
     }
 
 }
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -11,6 +12,10 @@ public class HomePage extends BasePage {
 
     private By searchInput = By.cssSelector(".header-search__field");
     private By languageChoice = By.xpath("//ul[@class='header__sub-lang']//li/a");
+    private By compareIcon=new By.ByCssSelector(".icon-compare-2-filled");
+    private By productCatalog=new By.ByXPath("//a[@class='jslink button']");
+    private By favoriteIcon = new By.ByCssSelector(".header-favorite__icon.header-favorite__icon_hover.icon-heart-filled");
+
 
 
     public HomePage(WebDriver driver) {
@@ -27,6 +32,7 @@ public class HomePage extends BasePage {
 
     public HomePage setLanguage(String language) {
         List<WebElement> elementList = driver.findElements(languageChoice);
+        wait.until(ExpectedConditions.visibilityOfAllElements(elementList));
         if (elementList.get(0).getText().equals(language)) {
             elementList.get(0).click();
         } else if (elementList.get(1).getText().equals(language)) {
@@ -34,5 +40,21 @@ public class HomePage extends BasePage {
         }
         return new HomePage(driver);
     }
+    public AllCategoriesPage compareAllCategory(){
+        WebElement searchCompareIcon=driver.findElement(compareIcon);
+        searchCompareIcon.click();
+        WebElement searchToCatalogButton=driver.findElement(productCatalog);
+        searchToCatalogButton.click();
+        return new AllCategoriesPage(driver);
+    }
+    public AllCategoriesPage favoriteAllCategory(){
+        WebElement searchCompareIcon=driver.findElement(favoriteIcon);
+        searchCompareIcon.click();
+        WebElement searchToCatalogButton=driver.findElement(productCatalog);
+        searchToCatalogButton.click();
+        return new AllCategoriesPage(driver);
+    }
+
+
 
 }
