c3f978fd217042df7a94201922560e76ae46fa0d
PikhulyaYana <pikhulyayana@gmail.com>
2022-07-05 22:34:56 +0300

    Added a test for adding two products to the cart.

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,10 +1,12 @@
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CartPage;
 import pages.HomePage;
 import pages.ProductPage;
 import pages.SearchResultsPage;
+
+import static utils.Category.LAPTOPS;
+import static utils.Subcategories.LAPTOPS_APPLE;
 
 
 public class CartTest extends BaseTest {
@@ -28,4 +30,54 @@ public class CartTest extends BaseTest {
         Assert.assertEquals(actualText, expectedText);
     }
 
+    @Test(groups = {"positive"})
+    public void checkFeatureButton() {
+        HomePage homePage = new HomePage(driver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+
+        int testProductIndex = 0;
+
+        homePage.catalogButtonClick();
+        homePage.clickSubCategory(LAPTOPS, LAPTOPS_APPLE);
+        searchResultsPage.clickProductByIndex(testProductIndex);
+        productPage.clickFeatureButton();
+        Assert.assertNotNull(productPage.getFeatureText());
+    }
+
+    @Test(dataProvider = "setLanguage", groups = {"positive"})
+    public void changeLanguageOnSite(String language, String expectedSearchPlaceholder) {
+        HomePage homePage = new HomePage(driver);
+        homePage.setLanguage(language);
+        String actualSearchPlaceholder = homePage.getSearchPlaceHolder();
+        Assert.assertEquals(actualSearchPlaceholder, expectedSearchPlaceholder);
+    }
+
+
+    @Test(groups = {"positive"})
+    public void addTwoProductsToCartTest() {
+        HomePage homePage = new HomePage(driver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = new CartPage(driver);
+
+        String searchCategory1 = "Кондиціонер";
+        String searchCategory2 = "Праски";
+        int testProductIndex = 0;
+
+        homePage.search(searchCategory1);
+        searchResultsPage.clickProductByIndex(testProductIndex);
+        String expectedText1 = productPage.getTitle();
+        productPage.buy();
+        String actualText1=cartPage.getTitles().get(0);
+
+        homePage.search(searchCategory2);
+        searchResultsPage.clickProductByIndex(testProductIndex);
+        String expectedText2 = productPage.getTitle();
+        productPage.buy();
+        String actualText2=cartPage.getTitles().get(1);
+
+        Assert.assertEquals(actualText1, expectedText1);
+        Assert.assertEquals(actualText2, expectedText2);
+    }
 }
