d00af60117d8436364a8ae1b4eae96f590300c3f
Kyrylo Bokhan <apllstar11@gmail.com>
2022-07-05 13:36:09 +0300

    Changed methods for sub category

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -11,6 +12,10 @@ public class HomePage extends BasePage {
 
     private By searchInput = By.cssSelector(".header-search__field");
     private By languageChoice = By.xpath("//ul[@class='header__sub-lang']//li/a");
+    private By catalogButton = By.xpath("//nav[@class='header__categories-catalog js-open-catalog-mb']");
+    private By catalog = By.xpath("//ul[@class='catalog__category smooth-scroll']");
+/*    public static By elementOfCategory = By.xpath("//main[@role = 'main']//li[@data-category='32021']");
+    public static By nestedElementOfCategory = By.xpath("//main[@role = 'main']//a[contains(@href,'noutbuki_apple.html')]");*/
 
 
     public HomePage(WebDriver driver) {
@@ -26,13 +31,29 @@ public class HomePage extends BasePage {
     }
 
     public HomePage setLanguage(String language) {
-        List<WebElement> elementList = driver.findElements(languageChoice);
-        if (elementList.get(0).getText().equals(language)) {
-            elementList.get(0).click();
-        } else if (elementList.get(1).getText().equals(language)) {
-            elementList.get(1).click();
+        List<WebElement> languages = driver.findElements(languageChoice);
+        if (languages.get(0).getText().equals(language)) {
+            languages.get(0).click();
+        } else if (languages.get(1).getText().equals(language)) {
+            languages.get(1).click();
         }
         return new HomePage(driver);
     }
 
+    public HomePage catalogButtonClick() {
+        driver.findElement(catalogButton).click();
+        return new HomePage(driver);
+    }
+
+    public By getLinkLocator(String sublink) {
+        By xPath = By.xpath("//main[@role = 'main']//a[contains(@href,'" + sublink + "')]");
+        return xPath;
+    }
+
+    public SearchResultsPage clickSubCategory(String category, String subcategory) {
+        driver.findElement(getLinkLocator(category)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(getLinkLocator(subcategory)));
+        driver.findElement(getLinkLocator(subcategory)).click();
+        return new SearchResultsPage(driver);
+    }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,10 +1,12 @@
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CartPage;
 import pages.HomePage;
 import pages.ProductPage;
 import pages.SearchResultsPage;
+
+import static utils.CategoryList.LAPTOPS;
+import static utils.SubcategoriesList.LAPTOPS_APPLE;
 
 
 public class CartTest extends BaseTest {
@@ -23,9 +25,23 @@ public class CartTest extends BaseTest {
         searchResultsPage.clickProductByIndex(testProductIndex);
         String expectedText = productPage.getTitle();
         productPage.buy();
-        String actualText=cartPage.getTitles().get(0);
+        String actualText = cartPage.getTitles().get(0);
 
         Assert.assertEquals(actualText, expectedText);
     }
 
+    @Test(groups = {"positive"})
+    public void checkFeatureButton() {
+        HomePage homePage = new HomePage(driver);
+        SearchResultsPage searchResultsPage = new SearchResultsPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+
+        int testProductIndex = 0;
+
+        homePage.catalogButtonClick();
+        homePage.clickSubCategory(LAPTOPS, LAPTOPS_APPLE);
+        searchResultsPage.clickProductByIndex(testProductIndex);
+        productPage.featureButtonClick();
+        Assert.assertNotNull(productPage.getFeatureText());
+    }
 }
